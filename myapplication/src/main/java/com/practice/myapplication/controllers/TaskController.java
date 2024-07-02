35cf668776245f01de0a00578564aa9f04083514
Vaari.Gupta <vaari.gupta@precisely.com>
2024-07-03 03:14:15 +0530

    added path variable

--- myapplication/src/main/java/com/practice/myapplication/controllers/TaskController.java
+++ myapplication/src/main/java/com/practice/myapplication/controllers/TaskController.java
@@ -3,13 +3,19 @@ package com.practice.myapplication.controllers;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("tasks")
+@RequestMapping("/tasks")
 public class TaskController {
 
     @GetMapping
     public String GetTask()
     {
         return "get tasks";
+    }
+
+    @GetMapping(path = "/{id}")
+    public String GetTaskByID(@PathVariable String id)
+    {
+        return "get task for ID : " + id;
     }
 
     @PostMapping
