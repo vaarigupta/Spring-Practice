9b45831d96c011f21bc2d531688437bf5625bac3
Vaari.Gupta <vaari.gupta@precisely.com>
2024-07-04 01:34:19 +0530

    making the request param optional

--- myapplication/src/main/java/com/practice/myapplication/controllers/TaskController.java
+++ myapplication/src/main/java/com/practice/myapplication/controllers/TaskController.java
@@ -13,10 +13,11 @@ public class TaskController {
     }
 
     @GetMapping
-    public String GetTaskByPagination( @RequestParam(value = "page") int page,
-                                       @RequestParam(value = "limit") int limit)
+    public String GetTaskByPagination( @RequestParam(value = "page", defaultValue = "1", required = false) int page, //defaultvalue makes the param optional by setting the default value
+                                       @RequestParam(value = "limit", required = false) Integer limit, //we have used wrapper class for int instead of primitive type because required parameter sets the default value to be null which is not possible in case of primitive data type, only possible with objects
+                                       @RequestParam(value="sort", defaultValue = "desc") String sort)
     {
-        return "get tasks for page : " + page + " and limit :" + limit;
+        return "get tasks for page : " + page + ", limit :" + limit + ", sort : " + sort;
     }
 
     @GetMapping(path = "/{id}")
