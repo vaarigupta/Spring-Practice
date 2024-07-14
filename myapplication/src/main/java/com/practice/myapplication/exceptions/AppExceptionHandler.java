7c7204380504881dbec79e9731ef6a4ed727662d
Vaari.Gupta <vaari.gupta@precisely.com>
2024-07-15 03:13:55 +0530

    handle more than one exception class from single method

--- myapplication/src/main/java/com/practice/myapplication/exceptions/AppExceptionHandler.java
+++ myapplication/src/main/java/com/practice/myapplication/exceptions/AppExceptionHandler.java
@@ -13,8 +13,8 @@ import java.util.Date;
 @ControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<ExceptionResponse> HandleAppException(Exception ex, WebRequest webRequest)
+    @ExceptionHandler(value ={Exception.class} )
+    public ResponseEntity<ExceptionResponse> HandleAnyException(Exception ex, WebRequest webRequest)
     {
         String exceptionMessage = ex.getLocalizedMessage();
 
@@ -25,27 +25,27 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(value = NullPointerException.class)
-    public ResponseEntity<ExceptionResponse> HandleNullPointerException(NullPointerException ex, WebRequest webRequest)
+    @ExceptionHandler(value = {NullPointerException.class, TaskServiceException.class})
+    public ResponseEntity<ExceptionResponse> HandleSpecificException(Exception ex, WebRequest webRequest)
     {
-        String nullPointerExceptionMessage = ex.getLocalizedMessage();
+        String exceptionMessage = ex.getLocalizedMessage();
 
-        if(nullPointerExceptionMessage==null)
-            nullPointerExceptionMessage = ex.toString();
+        if(exceptionMessage ==null)
+            exceptionMessage = ex.toString();
 
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),nullPointerExceptionMessage);
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Specific - " +exceptionMessage);
         return new ResponseEntity<>(exceptionResponse,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(value = TaskServiceException.class)
-    public ResponseEntity<ExceptionResponse> HandleTaskServiceException(TaskServiceException ex, WebRequest webRequest)
-    {
-        String taskServiceExceptionMessage = ex.getLocalizedMessage();
-
-        if(taskServiceExceptionMessage ==null)
-            taskServiceExceptionMessage = ex.toString();
-
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), taskServiceExceptionMessage);
-        return new ResponseEntity<>(exceptionResponse,HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+//    @ExceptionHandler(value = TaskServiceException.class)
+//    public ResponseEntity<ExceptionResponse> HandleTaskServiceException(TaskServiceException ex, WebRequest webRequest)
+//    {
+//        String taskServiceExceptionMessage = ex.getLocalizedMessage();
+//
+//        if(taskServiceExceptionMessage ==null)
+//            taskServiceExceptionMessage = ex.toString();
+//
+//        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), taskServiceExceptionMessage);
+//        return new ResponseEntity<>(exceptionResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
 }
