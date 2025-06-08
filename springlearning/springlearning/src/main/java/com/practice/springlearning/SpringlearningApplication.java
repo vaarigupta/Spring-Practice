212b5bed28d840eb200c7723d1d90a8f3b9be7fd
vaari.gupta <vaarigupta24@gmail.com>
2025-06-08 17:33:49 +0530

    Added spring webflux project

---
+++ springlearning/springlearning/src/main/java/com/practice/springlearning/SpringlearningApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlearningApplication.class, args);
+	}
+
+}
